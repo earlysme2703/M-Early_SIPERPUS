6a49e9a91ea17a2c0d1bab76e58162df898f2d90
Putriamandahaifa <bayandj64@gmail.com>
2024-11-09 09:46:31 +0700

    spatie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-        
+
         $this->call([
-            BookshelfSeeder::class,
+            // BookshelfSeeder::class,
+            PermissionSeeder::class,
+            RoleSeeder  ::class,
+            UserSeeder::class,
           ]);
     }
 }
